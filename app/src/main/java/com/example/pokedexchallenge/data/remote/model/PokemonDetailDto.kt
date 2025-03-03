4968b7ea6bf86ab1bde430b98bb41ba98dc0c9b6
johannagoenagac <johannagoenagac@gmail.com>
2025-03-03 18:49:59 -0300

    add api and dtos

---
+++ app/src/main/java/com/example/pokedexchallenge/data/remote/model/PokemonDetailDto.kt
@@ -0,0 +1,30 @@
+package com.example.pokedexchallenge.data.remote.model
+
+data class PokemonDetailResponse(
+    val id: Int,
+    val name: String,
+    val sprites: Sprites,
+    val types: List<PokemonType>
+)
+
+data class Sprites(
+    val other: OtherSprites
+)
+
+data class OtherSprites(
+    val officialArtwork: OfficialArtwork
+)
+
+data class OfficialArtwork(
+    val front_default: String
+)
+
+data class PokemonType(
+    val type: TypeDetail
+)
+
+data class TypeDetail(
+    val name: String
+)
+
+
