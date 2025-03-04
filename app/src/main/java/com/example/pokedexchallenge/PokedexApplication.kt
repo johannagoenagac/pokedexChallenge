70fe6b60612a12c1b44fb8d3c55f726f19bbbd45
johannagoenagac <johannagoenagac@gmail.com>
2025-03-04 20:25:40 -0300

    add NetworkModule

--- app/src/main/java/com/example/pokedexchallenge/PokedexApplication.kt
+++ app/src/main/java/com/example/pokedexchallenge/PokedexApplication.kt
@@ -1,7 +1,13 @@
 package com.example.pokedexchallenge
 
 import android.app.Application
+import dagger.Module
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PokedexApplication:Application()
+class PokedexApplication : Application() {
+    @Override
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/pokedexchallenge/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.example.pokedexchallenge.di
+
+import com.example.pokedexchallenge.data.remote.PokeApiService
+import com.example.pokedexchallenge.utils.constants.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeApiService(retrofit: Retrofit): PokeApiService {
+        return retrofit.create(PokeApiService::class.java)
+    }
+}
