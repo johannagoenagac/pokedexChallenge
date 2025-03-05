7013221e69a9e1ef4f693327a08136bca77d2614
johannagoenagac <johannagoenagac@gmail.com>
2025-03-05 12:24:25 -0300

    add DatabaseModule

---
+++ app/src/main/java/com/example/pokedexchallenge/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.pokedexchallenge.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.pokedexchallenge.data.local.PokemonDatabase
+import com.example.pokedexchallenge.data.local.dao.PokemonDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providePokemonDatabase(@ApplicationContext context: Context): PokemonDatabase {
+        return Room.databaseBuilder(
+            context,
+            PokemonDatabase::class.java,
+            "pokemon_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokemonDao(pokemonDatabase: PokemonDatabase): PokemonDao {
+        return pokemonDatabase.pokemonDao()
+    }
+}
